0996cb687417ea794f49802c7761b4ae6a262e49
Patrik Maraczek <patrik.maraczek@thermofisher.com>
2022-11-15 17:33:38 +0100

    no need to specify retained modules

--- python/package_redirection/old_package/__init__.py
+++ python/package_redirection/old_package/__init__.py
@@ -1,9 +1,6 @@
 import sys
 
 #new package reimports from old package
+#NOTE: name of reimported module has to be the same as old one
 from new_package import new_module as old_module 
 sys.modules['old_package.old_module'] = old_module
-
-#all retained modules from old package
-from old_package import retained_module
-sys.modules['old_package.retained_module'] = retained_module
