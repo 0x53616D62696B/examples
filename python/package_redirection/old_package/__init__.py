d774af0231c9800d3e7f82743f455890cde395f1
Patrik Maraczek <patrik.maraczek@thermofisher.com>
2022-11-18 14:21:33 +0100

    comments added

--- python/package_redirection/old_package/__init__.py
+++ python/package_redirection/old_package/__init__.py
@@ -1,6 +1,16 @@
 import sys
 
+'''
 #new package reimports from old package
-#NOTE: name of reimported module has to be the same as old one
+NOTE: Reimported module name has to be the same as the old module name. 
+        Use import DIFFERENT_NAME as SAME_NAME instead
+'''
 from new_package import new_module as old_module 
 sys.modules['old_package.old_module'] = old_module
+
+'''
+If you want to include all modules in this directory with 'from MODULE import *' 
+import (bad) practice, you would have to specify it in __all__ list, otherwise 
+it will not import anything if we importing from package.
+'''
+#__all__ = ['tng', 'adapt']
