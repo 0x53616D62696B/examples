86e20dfcf3aab44ca1230530f20334990c45a089
Patrik Maraczek <patrik.maraczek@thermofisher.com>
2022-11-18 15:56:40 +0100

    Cleaned and closed.. Option 1 is the winner

--- python/package_redirection/old_package/__init__.py
+++ python/package_redirection/old_package/__init__.py
@@ -1,52 +1,46 @@
-import sys
 import os
+'''
+If you want to include all modules in this directory with 'from MODULE import *' 
+import (bad) practice, you would have to specify it in __all__ list, otherwise 
+it will not import anything if we importing from package.
+'''
+#__all__ = ['old_module', 'retained_module'] #LIST OF ALL MODULEES YOU WANT TO INCLUDE WITH *
+
 
 '''
+Option 0
 #new package reimports from old package
 NOTE: Reimported module name has to be the same as the old module name. 
         Use import DIFFERENT_NAME as SAME_NAME instead
 '''
-
 """
 from new_package import new_module as old_module 
 sys.modules['old_package.old_module'] = old_module
 """
 
 '''
-If you want to include all modules in this directory with 'from MODULE import *' 
-import (bad) practice, you would have to specify it in __all__ list, otherwise 
-it will not import anything if we importing from package.
-'''
-
-#__all__ = ['old_module', 'retained_module'] #LIST OF ALL MODULEES YOU WANT TO INCLUDE WITH *
-#__path__ = []
-
-
-'''
-Option 1)
-
-moznost cislo 1: hodit sem prvne aegir a pak az tuhle slozku (tecku nebomezeru stringu) - aneb kdyz to nenajde modul v
-aegir, zkusi to hledat zde..
+Option 1) - #! WINNER
+First search the new package, than the old one.
 '''
 #get_absolute_path = os.getcwd()
 __path__ = [os.getcwd() + '\\python\\package_redirection\\new_package', __path__[0]]
 
+
 '''
 Option 2)
-
-dalsi moznost upravit submodule search locations - THIS POSSIBILITy is.. weird.. :D  BUT IT IS WORKING!
-#! It is hardcoded.. Not good at all
+Change submodule search locations - THIS POSSIBILITy is.. weird.. :D  BUT IT IS WORKING!
 '''
 """
-__spec__.submodule_search_locations.append(__spec__.submodule_search_locations[0])
-__spec__.submodule_search_locations[0] = 'c:\\dev\\examples\\python\\package_redirection\\new_package'
+__spec__.submodule_search_locations = ['c:\\dev\\examples\\python\\package_redirection\\new_package', 
+                                        __spec__.submodule_search_locations[0]]
 """
+
+
 '''
 Option 3)
-
-testnout loader nebo finder
+Loader nebo finder
 '''
+# To use appropriate finder youu should basically use my Option 1, __path__.
 
 
 print('test')
-
