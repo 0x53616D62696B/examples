6780694b8649e113ce4de480c33622263bb0c9f7
Patrik Maraczek <patrik.maraczek@thermofisher.com>
2022-12-08 18:51:30 +0100

    partly completed.. need better example

--- python/when_static/main.py
+++ python/when_static/main.py
@@ -1,16 +1,48 @@
-import values
 
-# no need for static variables
+
+
+# need to use static variable!!
+def increment_your_value():
+    var_ = 10
+    print(var_)
+    var_ = var_ + 1
+
 i=0
 while i < 10:
-    values.change_your_value()
-    values.show_value()
-    i = i + 1
+    increment_your_value()
+    i = i + 1  
 
 
-# need to use static variables - #! NOT TRUE, how is it?
+
+# global variable used!!
+var_ = 10
+def increment_your_global_value():
+    global var_
+    print(var_)
+    var_ = var_ + 1
+
 i=0
 while i < 10:
-    values.var_ = values.var_ + 1
-    print(values.var_)
+    increment_your_global_value()
+    i = i + 1  
+
+
+# static variable used!!
+#! totally useless way!!!!!!! dont do that again!!! 
+#! static is good aproach only in C++
+# not a good example.. cant find anything else right now
+# perhaps use this if needed 
+# https://stackoverflow.com/questions/279561/what-is-the-python-equivalent-of-static-variables-inside-a-function
+class static:
+    var_ = 20
+
+def increment_your_static_value():
+    var = static
+    print(var.var_)
+    print(static.var_)
+    var.var_ = var.var_ + 1
+
+i=0
+while i < 10:
+    increment_your_static_value()
     i = i + 1  
