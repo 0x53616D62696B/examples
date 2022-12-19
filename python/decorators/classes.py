c8d604342c57d1e1a3475e193c0354305e1716a4
Patrik Maraczek <patrik.maraczek@thermofisher.com>
2022-12-19 17:40:19 +0100

    possibility without args, kwargs

--- python/decorators/classes.py
+++ python/decorators/classes.py
@@ -16,14 +16,14 @@ class ProcedureFramework():
         self.list_of_events = []
         self.dict_of_events = {}
     
-    #def append_to_timed_event_list(self, event_function:Callable, some_value: int):
-    def append_to_timed_event_list(self, *args, **kwargs): 
+    #def append_to_timed_event_list(self, *args, **kwargs):
+    def append_to_timed_event_list(self, some_value): 
 
         def decorator(func):
             self.list_of_events.append(TimedEvent(func)) 
 
             self.dict_of_events.update({func.__name__:TimedEvent(func)})
 
-            print(args[0]) 
+            print(some_value) 
             print('inside the decorator function')
         return decorator
