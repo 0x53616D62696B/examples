03c71e0986b096fb7f624f819244e83685d99428
Patrik Maraczek <patrik.maraczek@thermofisher.com>
2022-12-19 17:07:59 +0100

    decorators update

--- python/decorators/classes.py
+++ python/decorators/classes.py
@@ -1,16 +1,22 @@
+from typing import Callable
+
 class TimedEvent():
 
     def __init__(self, func):
         
         self.list_of_events = []
+        self.test_var = 0
+        self.func = func
 
+    def run_event(self):
+        self.func(self)
 
 class ProcedureFramework():
     def __init__(self):
         self.list_of_events = []
         self.dict_of_events = {}
     
-    def append_to_timed_event_list(self, event_function:callable):
+    def append_to_timed_event_list(self, event_function:Callable):
         #event_instance = TimedEvent(event_function)
         #self._list_of_events.append(event_instance)
         self.list_of_events.append(TimedEvent(event_function))
--- python/decorators/diff_module.py
+++ python/decorators/diff_module.py
@@ -4,8 +4,14 @@ first_procedure = ProcedureFramework()
 
 @first_procedure.append_to_timed_event_list
 def setup_negative_polarity_event(frameworked_procedure:ProcedureFramework): 
-    pass
+    print("I am decorated function")
+    frameworked_procedure.test_var = 10
 
 
 print(first_procedure.list_of_events[0].__class__.__name__)
 print(first_procedure.dict_of_events['setup_negative_polarity_event'])
+
+test = first_procedure.dict_of_events.get('setup_negative_polarity_event')
+test.run_event()
+print('hello')
+#print(first_procedure.test_var)
